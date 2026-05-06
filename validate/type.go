011805c362545018a7ec7aee5b7c99a9f9978a8b
Jose Cabrera <nemutagk@gmail.com>
2026-05-06 12:56:25 -0600

    The error where adding the validator type caused a panic if the value was null has been fixed. Now, it first validates if it is null, and if it is null, it checks if null values ​​are allowed within the options (nullable); if so, it passes the validation even if it is null.

--- validate/type.go
+++ validate/type.go
@@ -3,6 +3,7 @@ package validate
 import (
 	"fmt"
 	"reflect"
+	"slices"
 )
 
 func Type(input string, value any, payload map[string]any, options []string, sliceIndex string, errors map[string]interface{}, addError func(string, string, map[string]interface{}, string) map[string]interface{}, customeErrors map[string]string) map[string]interface{} {
@@ -27,8 +28,26 @@ func Type(input string, value any, payload map[string]any, options []string, sli
 		return errors
 	}
 
+	if value == nil {
+		if slices.Contains(options, "nullable") {
+			return errors
+		}
+
+		tmpError := fmt.Sprintf("El campo \"%s\" no puede ser nulo", input)
+
+		if sliceIndex != "" {
+			tmpError = fmt.Sprintf("El campo \"%s\" en la posición %s no puede ser nulo", input, sliceIndex)
+		}
+
+		tmpErrorKey := fmt.Sprintf("%s.type", input)
+		if customeError, exists := customeErrors[tmpErrorKey]; exists {
+			tmpError = customeError
+		}
+		errors = addError(input, "type", errors, tmpError)
+		return errors
+	}
+
 	var_type := reflect.TypeOf(value).String()
-	fmt.Println("var_type: ", var_type)
 	if var_type != options[0] {
 		tmpError := fmt.Sprintf("El tipo del campo \"%s\" no es \"%s\"", input, options[0])
 
